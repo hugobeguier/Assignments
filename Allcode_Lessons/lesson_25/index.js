5ddf952060781e4f0384d176b22248e5da44df3c
hugobeguier <55714788+hugobeguier@users.noreply.github.com>
2025-02-01 18:09:28 +0100

    Assignment 25 done

--- Allcode_Lessons/lesson_25/index.js
+++ Allcode_Lessons/lesson_25/index.js
@@ -17,8 +17,8 @@ app.get('/get-users-info', async (req, res) => {
 
 app.post('/create-user', async (req, res) => {
     const userData = req.body;
-
-    const user = await prisma.User.create({
+    console.log("CREATE USER");
+    const user = await prisma.user.create({
         data: {
             userName: userData.userName,
             password: userData.password,
@@ -27,10 +27,40 @@ app.post('/create-user', async (req, res) => {
             country: userData.country
         }
     });
-
     res.send({ user: user });
 });
 
+app.get('/get-entries', async (req,res) => {
+    const entries = await prisma.entry.findMany();
+    res.send(entries);
+});
+
+app.post('/create-entry', async (req, res) => {
+    const entryData = req.body;
+
+    const entry = await prisma.entry.create({
+        data : {
+            title: entryData.title,
+            content: entryData.content,
+            createdDate: entryData.createdDate
+        }
+    });
+
+    res.send({entry:entry});
+});
+
+app.get('/fetch-entry-by-id/:id', async (req, res) => {
+
+    const id = parseInt(req.params.id);
+   
+    const entry = await prisma.entry.findUnique({
+        where : {
+            id: id
+        }
+    })
+
+    res.send(entry);
+});
 
 app.listen(4000, () => {
     console.log("Server running.");
